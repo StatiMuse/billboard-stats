87c8b87ace7d8da9b8698d9f7aa667dc3798e797
Bao Vuong <vqbao@lv5.org>
2018-03-28 22:46:04 -0400

    stuff

---
+++ billboard_stats.py
@@ -0,0 +1,12 @@
+import billboard
+from datetime import date 
+
+
+def fetch_gender(artist_name):
+    pass 
+
+def fetch_genre(song_title):
+    pass 
+
+def format_artist_name(input_name):
+    pass 
