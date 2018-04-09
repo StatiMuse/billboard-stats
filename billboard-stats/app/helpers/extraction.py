08904104d7c8d803faedb45ebed631ff1054d058
Bao Vuong <vqbao@lv5.org>
2018-04-09 18:07:32 -0400

    fixes

--- billboard-stats/app/helpers/extraction.py
+++ billboard-stats/app/helpers/extraction.py
@@ -4,7 +4,7 @@ def extract_gender(artist_name):
     set_useragent('Python-urllib', 'any')
     result = next(i for i in search_artists(artist_name)['artist-list'])
     if result:
-        return result['gender'] if 'gender' in i else None
+        return result['gender'] if 'gender' in result else None
     return None 
 
 def extract_genre(song_title):
