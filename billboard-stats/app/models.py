618dc34f261cdaf08a5f7608bf8ade66ca74ce0e
Bao Vuong <vqbao@lv5.org>
2018-04-17 18:37:49 -0400

    db stuff

--- billboard-stats/app/models.py
+++ billboard-stats/app/models.py
@@ -7,6 +7,7 @@ from django.db import models
 # Create your models here.
 class Artist(models.Model):
     name = models.CharField(max_length=40)
+    gender = models.
 
 class Song(models.Model):
     title = models.CharField(max_length=40)
